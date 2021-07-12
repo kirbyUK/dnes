632bec69196f07e17b5b9dd208e332c02bf4987b
Alex Kerr <kirby@cpan.org>
2021-07-12 21:02:59 +0100

    Switch to SDL_PIXELFORMAT_ARGB8888

--- source/dnes/screen/screen.d
+++ source/dnes/screen/screen.d
@@ -47,7 +47,7 @@ public:
 
         _texture = SDL_CreateTexture(
             _renderer,
-            SDL_PIXELFORMAT_BGRA8888,
+            SDL_PIXELFORMAT_ARGB8888,
             SDL_TEXTUREACCESS_STREAMING,
             width,
             height
